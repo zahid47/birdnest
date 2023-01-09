ac8cb27011d251dc99079312762b454642a49c72
zahid <epiczahid@gmail.com>
2023-01-10 02:15:02 +0600

    empty state

--- client/components/Drones/Drones.tsx
+++ client/components/Drones/Drones.tsx
@@ -8,11 +8,20 @@ export default function Drones() {
 
   return (
     <article>
-      <h1 className="text-2xl font-bold mb-4">NDZ Violations in the last 10 mins</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        NDZ Violations in the last 10 mins
+      </h1>
       <ul>
-        {data.map((drone: any) => {
-          return <Drone key={drone.serialNumber._text} drone={drone} />;
-        })}
+        {data.length ? (
+          data.map((drone: any) => {
+            return <Drone key={drone.serialNumber._text} drone={drone} />;
+          })
+        ) : (
+          <p>
+            Hooray! No drones have violated the NDZ in the last 10 mins. Thanks
+            for keeping it clean choom!
+          </p>
+        )}
       </ul>
     </article>
   );
